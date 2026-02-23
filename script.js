37bb22fb29fa161136632778f75fcf172d1220b4
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-23 11:47:20 +0100

    script.js

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ window.addEventListener('scroll', function() {
         bulle.classList.remove('visible');
     }
 });
+function transitionVersServices() {
+    const bulle = document.getElementById('missionBubble');
+    const services = document.getElementById('servicesIcons');
+
+    // 1. On cache la bulle avec une animation
+    bulle.classList.add('bubble-hidden');
+
+    // 2. On attend un petit peu (300ms) et on affiche les icônes
+    setTimeout(() => {
+        services.classList.add('services-visible');
+    }, 300);
+}
