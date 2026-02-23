c080f36a51ebb1b052508967977f25f76980c0a9
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-23 12:05:29 +0100

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-function transitionVersServices() {
-    const bulle = document.getElementById('missionBubble');
-    const services = document.getElementById('servicesIcons');
-
-    if (bulle && services) {
-        // Effacement de la bulle
-        bulle.style.opacity = "0";
-        bulle.style.transform = "translate(-50%, -60%) scale(0.9)";
-
-        setTimeout(() => {
-            bulle.style.display = "none";
-            
-            // Affichage des services
-            services.style.display = "flex";
-            setTimeout(() => {
-                services.style.opacity = "1";
-            }, 50);
-        }, 500);
+window.addEventListener('scroll', function() {
+    const bulle = document.querySelector('.speech-bubble');
+    if (window.scrollY > 60) {
+        bulle.classList.add('visible');
+    } else {
+        bulle.classList.remove('visible');
     }
-}
+});
