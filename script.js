55666c7531ff2b5964920595c07b0b9812532820
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-23 10:45:02 +0100

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 window.addEventListener('scroll', function() {
-    // On récupère l'élément avec la classe speech-bubble
-    const bulleMission = document.querySelector('.speech-bubble');
-    
-    // Si on scroll de plus de 45px, la bulle arrive
-    if (window.scrollY > 45) {
-        bulleMission.classList.add('visible');
+    const bulle = document.querySelector('.speech-bubble');
+    if (window.scrollY > 50) {
+        bulle.classList.add('visible');
     } else {
-        bulleMission.classList.remove('visible');
+        bulle.classList.remove('visible');
     }
 });
