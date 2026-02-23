0fcf6085e88868a3bcfe3bfb7c46799385d3b286
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-23 10:50:50 +0100

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     const bulle = document.querySelector('.speech-bubble');
-    if (window.scrollY > 50) {
+    if (window.scrollY > 60) {
         bulle.classList.add('visible');
     } else {
         bulle.classList.remove('visible');
