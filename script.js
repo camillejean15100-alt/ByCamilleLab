6c5234e60eb7af327b5a2d8f0a5967c63c59c9dd
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-23 10:03:17 +0100

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 window.addEventListener('scroll', function() {
-    const laBulle = document.querySelector('.speech-bubble');
+    const bulle = document.querySelector('.speech-bubble');
     
-    // Si on a descendu de plus de 40 pixels
     if (window.scrollY > 40) {
-        laBulle.classList.add('visible');
+        bulle.classList.add('visible');
     } else {
-        laBulle.classList.remove('visible');
+        bulle.classList.remove('visible');
     }
 });
