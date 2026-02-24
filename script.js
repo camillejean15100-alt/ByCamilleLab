1c987a86d6a937a05ac64425c401e7712f2c36d7
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-24 12:11:24 +0100

    script.js

--- script.js
+++ script.js
@@ -18,22 +18,20 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // 2. Ta navigation (Switch de section)
 function changerPage(pageId) {
-    const acc = document.getElementById('section-accueil');
-    const proj = document.getElementById('section-projets');
-    const liens = document.querySelectorAll('header nav ul li a');
+    const sectionAccueil = document.getElementById('section-accueil');
+    const sectionProjets = document.getElementById('section-projets');
 
-    // Masquage / Affichage
     if (pageId === 'projets') {
-        acc.style.display = 'none';
-        proj.style.display = 'block';
+        sectionAccueil.style.display = 'none';
+        sectionProjets.style.display = 'block';
     } else {
-        proj.style.display = 'none';
-        acc.style.display = 'block';
+        sectionProjets.style.display = 'none';
+        sectionAccueil.style.display = 'block';
     }
 
-    // Gestion du trait vert (Active)
-    liens.forEach(link => link.classList.remove('active'));
-    if (event && event.currentTarget) {
-        event.currentTarget.classList.add('active');
-    }
+    // Gestion de la classe active pour le trait vert
+    document.querySelectorAll('header nav ul li a').forEach(link => {
+        link.classList.remove('active');
+    });
+    event.currentTarget.classList.add('active');
 }
