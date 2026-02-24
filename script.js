ae6954895739d75231e6703bd4d5915f7540cfcb
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-24 10:14:52 +0100

    script.js

--- script.js
+++ script.js
@@ -18,21 +18,3 @@ window.addEventListener('DOMContentLoaded', () => {
     }, 3500);
 });
 
-// --- CHANGEMENT DE PAGE ---
-function changerPage(nom) {
-    const acc = document.getElementById('section-accueil');
-    const proj = document.getElementById('section-projets');
-    const body = document.getElementById('site-body');
-    const linkAcc = document.getElementById('link-acc');
-    const linkProj = document.getElementById('link-proj');
-
-    if (nom === 'projets') {
-        acc.style.display = 'none';
-        proj.style.display = 'block';
-        body.style.background = "#F3E9C7"; // Fond beige pour les projets
-        linkProj.classList.add('active');
-        linkAcc.classList.remove('active');
-    } else {
-        location.reload(); // Recharge pour relancer l'animation d'accueil
-    }
-}
