7c1efaccf8d03f0956ff75be680f859c1fda676b
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-24 10:37:09 +0100

    script.js

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ window.addEventListener('DOMContentLoaded', () => {
     }, 3500);
 });
 
+function changerPage(page) {
+    // On récupère les deux sections
+    const accueil = document.getElementById('section-accueil');
+    const projets = document.getElementById('section-projets');
+    const titreH1 = document.querySelector('.main-title');
+
+    if (page === 'projets') {
+        // --- MODE PROJETS ---
+        accueil.style.display = 'none';      // Cache tout l'accueil
+        if(titreH1) titreH1.style.display = 'none'; // Cache le titre H1
+        
+        projets.style.display = 'block';     // Affiche la galerie
+        document.body.style.backgroundImage = "none"; // Nettoie le fond du body
+    } else {
+        // --- MODE ACCUEIL ---
+        accueil.style.display = 'block';     // Réaffiche l'accueil
+        if(titreH1) titreH1.style.display = 'block'; 
+        
+        projets.style.display = 'none';      // Cache la galerie
+        document.body.style.backgroundImage = "url('fondaccueil.jpg')";
+    }
+}
