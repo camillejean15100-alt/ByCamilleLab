a584a30e9c11968c468102dc2fe4c611de8c65e5
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-25 13:16:33 +0100

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 window.addEventListener('load', () => {
-    const bubble = document.getElementById('mission-bubble');
-    const cards = document.getElementById('cards-container');
-
-    // FORCE le départ à droite pour la bulle (ignore le -30px du CSS)
-    if (bubble) bubble.style.transform = "translateX(50px)";
-
-    // 1. LA BULLE (Etape 1)
+    // 1. La bulle
     setTimeout(() => {
-        if (bubble) bubble.classList.add('is-visible');
-    }, 400);
+        document.getElementById('mission-bubble').classList.add('is-visible');
+    }, 500);
 
-    // 2. LES CARTES (Etape 2)
+    // 2. Les cartes
     setTimeout(() => {
-        if (cards) cards.classList.add('is-visible');
-    }, 1600);
+        document.getElementById('cards-container').classList.add('is-visible');
+    }, 1800);
 });
