e4f88f16344d369a6e89470ba3238a63106c943c
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-25 12:30:59 +0100

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-window.onload = function() {
-    const bubble = document.getElementById('missionBubble');
-    const services = document.getElementById('servicesIcons');
+window.addEventListener('load', () => {
+    const container = document.getElementById('main-frame');
     
-    // Étape 1 : Faire apparaître la bulle
-    setTimeout(() => bubble.classList.add('visible'), 500);
-
-    // Étape 2 : Switch vers les cartes après 3s
+    // On attend un peu que la page soit prête
     setTimeout(() => {
-        bubble.style.display = 'none';
-        services.style.display = 'flex';
-    }, 3500);
-};
-
-function changerPage(page) {
-    if (page === 'projets') {
-        document.getElementById('section-accueil').style.display = 'none';
-        document.getElementById('section-projets').style.display = 'block';
-        document.body.style.backgroundImage = "url('voirmesprojets.jpg')";
-    } else {
-        window.location.reload();
-    }
-}
+        container.classList.add('reveal-all');
+    }, 500); 
+});
