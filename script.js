d20f2d82ca4b9c3d25afff33530ec58839506420
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-25 14:22:55 +0100

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-window.onload = function() {
-    const bubble = document.getElementById('bubble');
-    const cards = document.getElementById('cards');
 
-    // Après 3 secondes, on cache la bulle et on montre les cartes
-    setTimeout(() => {
-        bubble.style.opacity = '0';
-        setTimeout(() => {
-            bubble.style.display = 'none';
-            cards.style.opacity = '1';
-        }, 500);
-    }, 3000);
-};
