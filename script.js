ab455e93dad17cc0292b0a42027dc208e4b4ea04
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-25 13:19:31 +0100

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () => {
-    // 1. La bulle
+    // 1. Apparition de la bulle (0.5s)
     setTimeout(() => {
         document.getElementById('mission-bubble').classList.add('is-visible');
     }, 500);
 
-    // 2. Les cartes
+    // 2. Apparition des cartes (1.8s)
     setTimeout(() => {
         document.getElementById('cards-container').classList.add('is-visible');
     }, 1800);
