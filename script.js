f67e36ba85b2337cb79d16a35b18428690429d3f
ByCamilleLab <camillejean.15100@gmail.com>
2026-02-25 13:58:52 +0100

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+    const bubble = document.getElementById('missionBubble');
+    const cards = document.getElementById('cardsBox');
+
+    // 1. On laisse la bulle visible pendant 4 secondes
+    setTimeout(() => {
+        // 2. On fait disparaître la bulle
+        bubble.classList.add('fade-out');
+        
+        // 3. Juste après la disparition, on fait apparaître les cartes
+        setTimeout(() => {
+            cards.classList.add('show-cards');
+        }, 600); 
+
+    }, 4000); 
+});
