e0b20c892a0dfbbb1eb36906661b405064a88a4e
ByCamilleLab <camillejean.15100@gmail.com>
2026-03-03 16:10:04 +0100

    script.js

--- script.js
+++ script.js
@@ -2,20 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const bubble = document.getElementById('js-bubble');
     const services = document.getElementById('js-services');
 
-    // 1. Faire apparaître la bulle après un court délai
+    // Apparition de la bulle
     setTimeout(() => {
         bubble.classList.add('bubble--active');
     }, 500);
 
-    // 2. Après 3 secondes, on cache la bulle et on montre les cartes
+    // Transition vers les services
     setTimeout(() => {
-        // Disparition de la bulle
         bubble.classList.remove('bubble--active');
         
-        // Petit délai pour attendre la fin de l'anim de la bulle
         setTimeout(() => {
             bubble.style.display = 'none';
-            // Apparition des cartes décalées à droite
             services.classList.add('services--visible');
         }, 500);
 
