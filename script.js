769028435aa67ae77a55240b8631c59ec159b002
ByCamilleLab <camillejean.15100@gmail.com>
2026-03-04 12:04:37 +0100

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+    const bubble = document.getElementById('js-bubble');
+    const services = document.getElementById('js-services');
+
+    // Apparition de la bulle
+    setTimeout(() => {
+        bubble.classList.add('bubble--active');
+    }, 500);
+
+    // Transition vers les services
+    setTimeout(() => {
+        bubble.classList.remove('bubble--active');
+        
+        setTimeout(() => {
+            bubble.style.display = 'none';
+            services.classList.add('services--visible');
+        }, 500);
+
+    }, 3500);
+});
